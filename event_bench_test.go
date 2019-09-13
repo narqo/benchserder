d09632fe25f8e2105c9264a86be52b1806730777
Vladimir Varankin <vladimir@varank.in>
2019-09-13 22:09:52 +0200

    add basic acceptance tests for marshallers

--- event_bench_test.go
+++ event_bench_test.go
@@ -4,6 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 var testEvents = []string{
@@ -15,109 +18,32 @@ var testEvents = []string{
 	"event_225K.json",
 }
 
-//func TestEvent_MarshalUnmarshalJSON(t *testing.T) {
-//	for _, tf := range testEvents {
-//		t.Run(tf, func(t *testing.T) {
-//			data := mustReadFile(t, tf)
-//
-//			var event Event
-//			require.NoError(t, json.Unmarshal(data, &event))
-//
-//			gotData, err := json.Marshal(event)
-//			require.NoError(t, err)
-//			assert.JSONEq(t, string(data), string(gotData))
-//		})
-//	}
-//}
-//
-//func TestEvent_MarshalUnmarshalMsgpack(t *testing.T) {
-//	for _, tf := range testEvents {
-//		t.Run(tf, func(t *testing.T) {
-//			data := mustReadFile(t, tf)
-//			var event Event
-//			require.NoError(t, json.Unmarshal(data, &event))
-//
-//			t.Run("as struct", func(t *testing.T) {
-//				mData, err := marshalMsgpack(event)
-//				require.NoError(t, err)
-//
-//				err = unmarshalMsgpack(mData, &event)
-//				require.NoError(t, err)
-//
-//				gotData, _ := json.Marshal(event)
-//				assert.JSONEq(t, string(data), string(gotData))
-//			})
-//
-//			t.Run("as array", func(t *testing.T) {
-//				mData, err := marshalMsgpackAsArray(event)
-//				require.NoError(t, err)
-//
-//				err = unmarshalMsgpack(mData, &event)
-//				require.NoError(t, err)
-//
-//				gotData, _ := json.Marshal(event)
-//				assert.JSONEq(t, string(data), string(gotData))
-//			})
-//		})
-//	}
-//}
-//
-//func TestEvent_MarshalUnmarshalCodecCBOR(t *testing.T) {
-//	for _, tf := range testEvents {
-//		t.Run(tf, func(t *testing.T) {
-//			data := mustReadFile(t, tf)
-//			var event Event
-//			require.NoError(t, json.Unmarshal(data, &event))
-//
-//			cData, err := marshalCodecCBOR(event)
-//			require.NoError(t, err)
-//
-//			err = unmarshalCodecCBOR(cData, &event)
-//			require.NoError(t, err)
-//
-//			gotData, _ := json.Marshal(event)
-//			assert.JSONEq(t, string(data), string(gotData))
-//		})
-//	}
-//}
-//
-//func TestEvent_MarshalUnmarshalFxamackerCBOR(t *testing.T) {
-//	for _, tf := range testEvents {
-//		t.Run(tf, func(t *testing.T) {
-//			data := mustReadFile(t, tf)
-//			var event Event
-//			require.NoError(t, json.Unmarshal(data, &event))
-//
-//			cData, err := marshalFxamackerCBOR(event)
-//			require.NoError(t, err)
-//
-//			err = unmarshalFxamackerCBOR(cData, &event)
-//			require.NoError(t, err)
-//
-//			gotData, _ := json.Marshal(event)
-//			assert.JSONEq(t, string(data), string(gotData))
-//		})
-//	}
-//}
-//
-//func TestEvent_MarshalUnmarshalGogoProto(t *testing.T) {
-//	for _, tf := range testEvents {
-//		t.Run(tf, func(t *testing.T) {
-//			data := mustReadFile(t, tf)
-//			event := &Event{}
-//			require.NoError(t, json.Unmarshal(data, event))
-//
-//			pData, err := marshalGogoProto(event)
-//			require.NoError(t, err)
-//
-//			err = unmarshalGogoProto(pData, event)
-//			require.NoError(t, err)
-//
-//			gotData, _ := json.Marshal(event)
-//			assert.JSONEq(t, string(data), string(gotData))
-//		})
-//	}
-//}
+func TestEvent_MarshalUnmarshal(t *testing.T) {
+	for _, tc := range marshallers {
+		t.Run(fmt.Sprintf("marshaller=%s", tc.name), func(t *testing.T) {
+			tm := tc.marshaller()
+			for _, tf := range testEvents {
+				t.Run(fmt.Sprintf("file=%s", tf), func(t *testing.T) {
+					data := mustReadFile(t, tf)
+					event := &Event{}
+					require.NoError(t, json.Unmarshal(data, &event))
+
+					gotData, err := tm.marshalFunc(event)
+					require.NoError(t, err)
+					require.NotEmpty(t, gotData)
+
+					gotEvent := &Event{}
+					require.NoError(t, tm.unmarshalFunc(gotData, gotEvent))
+					//assert.Equal(t, event, gotEvent)
+
+					d1, _ := json.Marshal(event)
+					d2, _ := json.Marshal(gotEvent)
+					assert.JSONEq(t, string(d1), string(d2))
+				})
+			}
+		})
+	}
+}
 
 func BenchmarkEvent_Marshal(b *testing.B) {
 	for _, tc := range marshallers {
